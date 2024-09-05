ba7f0211b997a48c28e71b19526a4b9a1a2b9d7a
marcusslomka <marcus.slomka@googlemail.com>
2024-09-05 08:20:34 +0200

    CustomerController done

---
+++ src/main/java/de/supercode/tastaturen_shop/controllers/CustomerController.java
@@ -0,0 +1,43 @@
+package de.supercode.tastaturen_shop.controllers;
+
+import de.supercode.tastaturen_shop.entities.kunde.Customer;
+import de.supercode.tastaturen_shop.services.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/shop/customer")
+public class CustomerController {
+
+    CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createNewCustomer(@RequestBody Customer customer){
+        customerService.createNewCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerByID(@PathVariable long id){
+        customerService.getCustomer(id);
+        if (customerService.getCustomer(id).isEmpty()) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        else return ResponseEntity.status(HttpStatus.FOUND).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable long id, @RequestBody Customer customer){
+        customerService.updateCustomer(id,customer);
+        if (customerService.updateCustomer(id,customer) == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        else return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable long id){
+        customerService.deleteCustomer(id);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/de/supercode/tastaturen_shop/services/CustomerService.java
+++ src/main/java/de/supercode/tastaturen_shop/services/CustomerService.java
@@ -2,7 +2,11 @@ package de.supercode.tastaturen_shop.services;
 
 import de.supercode.tastaturen_shop.entities.kunde.Customer;
 import de.supercode.tastaturen_shop.repositories.CustomerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -14,11 +18,23 @@ public class CustomerService {
     }
 
     public void createNewCustomer(Customer customer){
+
         this.customerRepository.save(customer);
     }
-    public Customer getCustomer(long id){
-        Customer customer = this.customerRepository.findById(id).orElse(null);
-        if (customer == null) return null;
-            else return customer;
+    public Optional<Customer> getCustomer(long id){
+        return this.customerRepository.findById(id);
     }
+    public Customer updateCustomer(long id, Customer customer){
+        Customer toUpdateCustomer = customerRepository.findById(id).orElse(null);
+        if (customer == null) return null;
+        toUpdateCustomer.setAddress(customer.getAddress());
+        toUpdateCustomer.setEmail(customer.getEmail());
+        toUpdateCustomer.setFirstName(customer.getFirstName());
+        toUpdateCustomer.setLastName(customer.getLastName());
+        return customerRepository.save(toUpdateCustomer);
+    }
+    public void deleteCustomer(long id){
+        customerRepository.deleteById(id);
+    }
+
 }
